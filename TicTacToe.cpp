7a703402a5a0bd36f2faae0c77c179b59215a408
Sergio Nogueira Jr <59852836+sergionjr@users.noreply.github.com>
2021-09-25 10:50:35 -0600

    GetPlayerChoice Function

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -16,7 +16,7 @@ struct Board{
 void CreateBoard (Board &board){
     for (int i = 0; i < 3; i++){
         for (int j = 0; j < 3; j++){
-            board.square[i][j] = Empty;
+            board.square[i][j] = SquareClass::Empty;
             
         }
     }
@@ -26,12 +26,34 @@ void CreateBoard (Board &board){
 void DisplayBoard(const Board &b){
     std::cout << "   1  2  3" << std::endl; 
     for (int i = 0; i < 3; i++){
-        std::cout << i+1 << " ";
         for (int j = 0; j < 3; j++){
-            if board
-            std::cout << b.square[i][j] + " ";
+            if (board[i][j] == SquareClass::Empty){std::cout<<"~ ";}
+            if (board[i][j] == SquareClass::X){std::cout<<"X ";}
+            if (board[i][j] == SquareClass::O){std::cout<<"O ";}
         }
         std::cout << std::endl;
+    }
+}
+
+void GetPlayerChoice(Board &b){
+    b.row = -1;
+    b.col = -1;
+    std::cout << "Select a row and column to place your marker." << std::endl;
+    std::cout << "Row: ";
+    std::cin >> b.row;
+    b.row--;
+    while (b.row < 0 || b.row > 2){
+        std::cout << "This is not a valid square. Please enter a number from 1 to 3 that does not already have an X or O inside of it." << std::endl;
+        std::cin >> b.row;
+        b.row--;
+    }
+    std::cout << "Column: ";
+    std::cin >> b.col;
+    b.col--;
+    while (b.col < 0 || b.col > 2){
+        std::cout << "This is not a valid square. Please enter a number from 1 to 3 that does not already have an X or O inside of it." << std::endl;
+        std::cin >> b.col;
+        b.col--;
     }
 }
 
